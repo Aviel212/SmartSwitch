d61d9a2291233b932de1a5433b39a74d41b90689
Aviel212 <Aviel9731@gmail.com>
2019-03-28 18:48:41 +0200

    add class Task
    added class Task - for execute some action in certain time

---
+++ src/Server/Task.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/// <summary>
+/// Task class for execute some action in certain time
+/// </summary>
+public class Task
+{
+    enum Operations { TURNON, TURNOFF };
+
+    public Operations Operation;
+    public Plug Device;
+
+    public Task(Operations op, Plug dev)
+	{
+		//need to set properties 
+	}
+
+    public void Execute()
+    {
+        //when entering this function we need to execute the task
+    }
+}
