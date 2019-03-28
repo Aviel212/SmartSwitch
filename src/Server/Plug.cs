5f05ddd36e4a3890e0e618990239b60f4911ab54
Aviel212 <Aviel9731@gmail.com>
2019-03-28 18:42:07 +0200

    added 2 classes - Plug and PowerUsageSample
    added class representing a smart device which you can turn on and off and use simple tasks on it - Plug
    
    added class for current and voltage sample of a given device at a given time

---
+++ src/Server/Plug.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+/// <summary>
+/// this class representing a smart device which you can turn on and off and use simple tasks on it
+/// </summary>
+public class Plug
+{
+    enum Priorities {ESSENTIAL, NONESSENTIAL, IRRELEVANT};
+
+    public readonly string Mac;
+    public string NickName;
+    public bool IsOn;
+    public bool Approved;
+    public Priorities Priority;
+
+    public Plug(string mac)
+	{
+        Mac = mac;
+        IsOn = false;
+        Approved = false;
+        Priority = Priorities.IRRELEVANT;
+	}
+
+    public void TurnOn()
+    {
+        //turn on the device
+    }
+
+    public void TurnOff()
+    {
+        //turn off the device
+    }
+
+    public void addSample(PowerUsageSample pus)
+    {
+        //add a new sample to the list of samples
+    }
+
+}
---
+++ src/Server/PowerUsageSample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/// <summary>
+/// A current and voltage sample of a given device at a given time
+/// </summary>
+public class PowerUsageSample
+{
+    public readonly double Current;
+    public readonly DateTime SampleDate;
+    public readonly double Voltage;
+
+	public PowerUsageSample(double c, DateTime dt, double v)
+	{
+        Current = c;
+        SampleDate = dt;
+        Voltage = v;
+	}
+
+    public double GetWattage()
+    {
+        return Current * Voltage;
+    }
+}
