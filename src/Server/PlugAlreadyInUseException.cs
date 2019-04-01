01bb412e8d29dd7e1bb8f7e27676e453c853c16b
RonKatz1 <45691158+RonKatz1@users.noreply.github.com>
2019-04-01 17:04:27 +0300

    added 3 classes- ConnectionMnager and 2 exceptions
    ConnectionMnager - a singleton class that manages newly created  plugs and users to the system
    
    PlugAlreadyInUseException -
    an exception that is indicating that the plug is already taken
    
    UsernameAlreadyInUseException -
     an exception that is indicating that the username is already taken

---
+++ src/Server/PlugAlreadyInUseException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class PlugAlreadyInUseException : Exception
+{
+    public PlugAlreadyInUseException()
+    {
+    }
+
+    public PlugAlreadyInUseException(string message)
+        : base(message)
+    {
+    }
+
+    public PlugAlreadyInUseException(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+}
