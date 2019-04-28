62222420ec2db4c92121609123255b5a14b4b5e5
YardenShoham <hrsi88@gmail.com>
2019-04-28 22:13:08 +0300

    delete unnecessary folder

--- src/Server/ServerSide/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ServerSide.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- src/Server/ServerSide/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ServerSide.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
